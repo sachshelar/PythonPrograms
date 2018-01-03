a83f4ffc37a46eec68ce93de3a4d73eb0d8ba2cc
sachin.shear <sachin.shelar@ecotechservices.com>
2018-01-03 23:36:12 +0530

    Update For Range Program

--- eg_for_range.py
+++ eg_for_range.py
@@ -1,5 +1,20 @@
+#This method is used to print @ in format
 def for_range_example(width,height):
-    print("@"*width)
-    #use of for for with range
+    print("*"*width)
+    """print a box made up of asterisks.
 
-for_range_example(2,4)
+    width: width of box in characters, must be at least 2
+    height: height of box in lines, must be at least 2
+    """
+
+
+    # print sides of box
+    for _ in range(height-2):
+        print("*" + " " * (width-2) + "*")
+
+
+    print("*" * width)
+
+
+
+for_range_example(4,7)
